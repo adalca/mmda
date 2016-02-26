d8ce553237e952005f14c18a7cb5e7541be2e668
adalca <adalca@mit.edu>
2016-02-26 18:57:32 -0500

    expand modality check

--- src/@medicalDataset/boundingBox.m
+++ src/@medicalDataset/boundingBox.m
@@ -2,6 +2,11 @@ function [croppedVol, cropMask, cropArray, bBox] = ...
         boundingBox(md, inputModality, outputModality, varargin)
     % bounding boxes
 
+    mods = {inputModality};
+    if nargin > 2 && ~isempty(outputModality)
+        mods = {inputModality, outputModality};
+    end
+    
     [croppedVol, cropMask, cropArray, bBox] = ...
-        md.applyfun(@boundingBoxNii, {inputModality, outputModality}, varargin{:});
+        md.applyfun(@boundingBoxNii, mods, varargin{:});
 end
